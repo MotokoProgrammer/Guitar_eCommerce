ba368b00be43175bc87f7ef35e916627be0f78a1
David Lee <DavidLee8two7@gmail.com>
2018-09-22 13:21:03 -0700

    hasing the user password with bcrypt

--- server/models/user.js
+++ server/models/user.js
@@ -1,43 +1,63 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const SALT_I = 10;
 
 const userSchema = mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: 1
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5
-  },
-  name: {
-    type: String,
-    required: true,
-    maxlength: 30
-  },
-  lastname: {
-    type: String,
-    required: true,
-    maxlength: 30
-  },
-  cart: {
-    type: Array,
-    default: []
-  },
-  history: {
-    type: Array,
-    default: []
-  },
-  role: {
-    type: Number,
-    default: 0
-  },
-  token: {
-    type: String
-  }
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    name: {
+        type: String,
+        required: true,
+        maxlength: 30
+    },
+    lastname: {
+        type: String,
+        required: true,
+        maxlength: 30
+    },
+    cart: {
+        type: Array,
+        default: []
+    },
+    history: {
+        type: Array,
+        default: []
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    token: {
+        type: String
+    }
 });
+
+userSchema.pre('save',function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(SALT_I,function(err,salt){
+            if(err) return next(err);
+    
+            bcrypt.hash(user.password,salt,function(err,hash){
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            });
+        })
+    } else{
+        next()
+    }
+})
 
 const User = mongoose.model("User", userSchema);
 
