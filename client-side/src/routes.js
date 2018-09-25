92548580f268ea6a56669e38e3724088318ff6c5
David Lee <DavidLee8two7@gmail.com>
2018-09-25 15:57:14 -0700

    creating register login part

--- client-side/src/routes.js
+++ client-side/src/routes.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import Layout from './hoc/Layout';
 import Home from './components/Home/Home';
+import RegisterLogin from './components/Register_login/Register_login';
 
 const Routes = () => (
     <Layout>
         <Switch>
-            <Route path="/" exact component={Home} />
+            <Route path='/register_login' exact component={RegisterLogin} />
+            <Route path='/' exact component={Home} />
         </Switch>
     </Layout>
 );
